885ce8863c714af18972de19ff33e422e748bdcf
Garrett Martin <martingv19@gcc.edu>
2023-04-19 20:13:31 -0400

    stuff

---
+++ Driver.py
@@ -0,0 +1,44 @@
+from Board import board
+from Piece import piece
+import copy
+
+def main():
+    gameBoard: board = board()
+
+
+
+    while True:
+        gameBoard = minimax(gameBoard)
+
+        if gameBoard.isFull:
+            break
+
+
+    boardList: list[board] = list()
+
+    for spots in gameBoard.getAllPlayableSpots():
+        newBoard = copy.deepcopy(gameBoard)
+        if len(spots) == 1: continue
+        for row,col in spots:
+            newBoard.gameBoard[row][col].color = 'W'
+        boardList.append(newBoard)
+
+
+    for b in boardList:
+        print(b)
+
+
+
+def minimax(game: board) -> board:
+
+    
+
+    return game
+
+
+
+
+
+
+
+main()
