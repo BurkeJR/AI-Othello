096bda9d04c801f227bf0ddca887c0d57998f866
Garrett Martin <martingv19@gcc.edu>
2023-04-30 22:02:00 -0400

    teioa

--- Evaluation.py
+++ Evaluation.py
@@ -1,16 +1,11 @@
 from AIDriver import aiDriver
-from Heuristic import coinParity, cornersCaptured, mobility, stability
+from Heuristic import coinParity, cornersCaptured, mobility, stability, combination
 import time
 
 def main():
     bAI = [coinParity(), cornersCaptured(), mobility(), stability()]
-    wAI = [coinParity(), cornersCaptured(), mobility(), stability()]
-    results = [['' for _ in range(5)] for _ in range(5)]
-    results[0] = ['', str(wAI[0]), str(wAI[1]), str(wAI[2]), str(wAI[3])]
-    results[1][0] = str(bAI[0])
-    results[2][0] = str(bAI[1])
-    results[3][0] = str(bAI[2])
-    results[4][0] = str(bAI[3])
+    wAI = [combination()]
+    results = [['' for _ in range(1)] for _ in range(4)]
 
     start = time.time()
 
@@ -19,7 +14,7 @@ def main():
             blackAI = bAI[i]
             whiteAI = wAI[j]
             driver = aiDriver(blackAI, whiteAI, MAX_DEPTH=5, wordy=False)
-            results[i+1][j+1] = driver.run()
+            results[i][j] = driver.run()
     end = time.time()
     print(results)
     print(f"Runtime = {end - start}")
